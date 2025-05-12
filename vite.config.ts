46a046daea40d76c87ceb9a70d08465e994c64fc
Alexstones <168788729+Alexstones@users.noreply.github.com>
2025-05-12 18:03:37 +0000

    git status

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       $components: '/src/lib/components',
     },
   },
+  server: {
+    host: true, // Asegúrate de que Vite escuche en todas las interfaces
+    port: 5173, // El puerto que Vite está utilizando (puede variar)
+    allowedHosts: [
+      'localhost',
+      '9befbf13-f2c3-41a3-aff2-ce7fc1f72c18-00-2i8hvjrsyoudn.kirk.replit.dev'
+    ]
+  }
 });
